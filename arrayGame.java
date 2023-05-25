cfd37ba719e0a17ad2bcfe3f42321f08e85f5dcc
con-orr <jackco@limestone.on.ca>
2023-05-25 09:17:09 -0400

    first commit

--- arrayGame.java
+++ arrayGame.java
@@ -1,3 +1,9 @@
-public class arrayGame{
-    
+import java.util.Scanner;
+
+public class arrayGame {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int points = 0;
+
+    }
 }
