0e4e9f8859d83a915ee48b778604c5ee26d97597
con-orr <jackco@limestone.on.ca>
2023-05-26 09:30:54 -0400

    update array and pulls a random word at the beginning

--- arrayGame.java
+++ arrayGame.java
@@ -5,19 +5,21 @@ public class arrayGame {
         Scanner in = new Scanner(System.in);
          int wordLength = 5;
          int attempts = 0;
+         String secretWord = generateSecretWord(5); //pulls a random word from the array to be used for the current round
 
          while (attempts < 10) { 
             System.out.println("Enter your guess");
             String guess = in.nextLine();
             if (guess.length() != wordLength) {
                 System.out.println("invaild guess! please input a word with " + wordLength + " characters");
+                continue;
             }
 
          }
 
     }
     public static String generateSecretWord(int wordLength) {
-        String[] words = {"apple", "pears", "peach", "lemon", "grape" };
+        String[] words = {"apple", "pears", "peach", "lemon", "grape", "melon", "kiwis" };
         int randomPicker = (int) (Math.random()*words.length);
         return words[randomPicker];
     }
