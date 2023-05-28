51af87dad2016977f14cab8193675bf13d06f8f9
con-orr <jackco@limestone.on.ca>
2023-05-28 17:35:38 -0400

    remove ending
    broken, will be fixing

--- arrayGame.java
+++ arrayGame.java
@@ -4,44 +4,47 @@ public class arrayGame {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         boolean EndGame = false;
-         int wordLength = 5;
-         int attempts = 0;
-         
-         String secretWord = generateSecretWord(5); //pulls a random word from the array to be used for the current round
-         System.out.println("welcome to foodle, a food based wordle game, you have 10 attempts to guess the word. misplaced letters are printed as * and incorrect letters are -, every secret word only has 5 letters! good luck!");
+        int wordLength = 5;
+        int attempts = 0;
 
-         while (attempts < 10) { 
+        String secretWord = generateSecretWord(5); // pulls a random word from the array to be used for the current
+                                                   // round
+        System.out.println(
+                "welcome to foodle, a food based wordle game, you have 10 attempts to guess the word. misplaced letters are printed as * and incorrect letters are -, every secret word only has 5 letters! good luck!");
+
+        while (attempts < 10) {
             System.out.println("Enter your guess");
             String guess = in.nextLine();
             if (guess.length() != wordLength) {
                 System.out.println("invaild guess! please input a word with " + wordLength + " characters");
-                
-            } 
-            if (guess.equals(secretWord)){
-                System.out.println("You guessed the correct word!");
-                EndGame = true;
+
             }
-             else {
+            if (guess.equals(secretWord)) {
+                System.out.println("You guessed the correct word!");
+
+            } else {
                 String feedback = provideFeedback(guess, secretWord);
                 System.out.println(feedback);
                 attempts++;
 
-         }
-          if (attempts == 10) {
-            System.out.println("You couldn't guess the word. The secret word was: " + secretWord);
-        }
-        if (EndGame = true){
-            in.close();
+            }
+            if (attempts == 10) {
+                System.out.println("You couldn't guess the word. The secret word was: " + secretWord);
+            }
+           
+
         }
     }
 
-    }
+    
+
     public static String generateSecretWord(int wordLength) {
         String[] words = {"apple", "pears", "peach", "lemon", "grape", "melon", "kiwis" };
         int randomPicker = (int) (Math.random()*words.length);
         return words[randomPicker];
     }
-     public static String provideFeedback(String guess, String secretWord) {
+
+    public static String provideFeedback(String guess, String secretWord) {
         String feedback = "";
 
         for (int i = 0; i < guess.length(); i++) {
